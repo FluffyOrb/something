6acb395971e4678ec6ebc6853e23c33ef6a48ee6
Jakub Wisnicki <jk.wisnicki@gmail.com>
2021-10-04 21:57:14 +0200

    Add footer content

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AppConsts } from 'src/app/consts/app.consts';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  readonly APP_NAME = AppConsts.APP_NAME;
+  readonly APP_CREATOR = AppConsts.APP_CREATOR;
+  readonly APP_CREATOR_GITHUB_URL = AppConsts.APP_CREATOR_GITHUB_URL;
 
 }
---
+++ src/app/consts/app.consts.ts
@@ -0,0 +1,7 @@
+export class AppConsts {
+
+  static readonly APP_NAME = 'Something';
+  static readonly APP_CREATOR = 'FluffyOrb';
+  static readonly APP_CREATOR_GITHUB_URL = 'https://github.com/FluffyOrb';
+
+}
